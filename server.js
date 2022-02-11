a73c13908d85cb75c941fe2150d378cec9f5d9cc
Parn <pan.tatchanon@gmail.com>
2022-02-12 02:38:48 +0700

    Connect with MongoDB Atlas

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
+//Connect to database
+connectDB();
 
+//Route files
 const app=express();
-
+const hospitals = require ('./routes/hospitals');
 // app.get('/', (req,res) => {
 //     //res.send('<h1>Hello from express</h1>');
 //     //res.send({name:'Brad'});
@@ -15,8 +19,16 @@ const app=express();
 //     res.status(200).json({success:true, data:{id:1}});
 // });
 
-const hospitals = require ('./routes/hospitals');
+//Body paraer
+app.use(express.json());
 app.use('/api/v1/hospitals', hospitals)
 
 const PORT=process.env.PORT || 3000;
-app.listen(PORT, console.log('Server running in ',process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(PORT, console.log('Server running in ',process.env.NODE_ENV, ' mode on port ', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection',(err,Promise)=>{
+    console.log(`Error: ${err.message}`);
+    //Close server & exot process
+    server.close(()=> process.exit(1));
+});
