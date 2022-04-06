fd3b235ceb5a104a9ada06ce99bdfa9f12ea101e
Parn <pan.tatchanon@gmail.com>
2022-04-06 15:17:17 +0700

    add Swagger

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const xss=require('xss-clean');
 const rateLimit=require('express-rate-limit');
 const hpp=require('hpp');
 const cors=require('cors');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -28,6 +30,26 @@ const appointments = require('./routes/appointments');
 //     //res.status(400).json({success:false});
 //     res.status(200).json({success:true, data:{id:1}});
 // });
+
+const swaggerOptions={
+    swaggerDefinition:{
+    openapi: '3.0.0',
+    info: {
+        title: 'Library API',
+        version: '1.0.0',
+        description: 'A simple Express VacQ API'
+        },
+        servers:[
+            {
+                url: 'http://localhost:3000/api/v1'
+            }
+        ],
+    },
+    apis:['./routes/*.js'],
+    };
+
+const swaggerDocs=swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 
 //Cookie parser
